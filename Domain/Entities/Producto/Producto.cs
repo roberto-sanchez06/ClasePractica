05502ed4b69f4e648446f8c3c641485fd9269ba2
roberto-sanchez06 <sanrobb18@gmail.com>
2021-10-05 23:17:42 -0600

    Añadiendo clases

---
+++ Domain/Entities/Producto/Producto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Enums.Producto;
+
+namespace Domain.Entities.Producto
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public decimal Precio { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public UnidadMedida UnidadMedida { get; set; }
+
+        public class ProductoOrderByPrecio : IComparer<Producto>
+        {
+            public int Compare(Producto x, Producto y)
+            {
+                if (x.Precio < y.Precio)
+                {
+                    return -1;
+                }
+                else if (x.Precio > y.Precio)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+}
