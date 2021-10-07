4957d576daf4d54e68230eb31f19f690d593ace4
roberto-sanchez06 <sanrobb18@gmail.com>
2021-10-07 09:43:49 -0600

    Agregando interfaz y metodo factory

---
+++ AppCore/Factories/ValoracionInventarioFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AppCore.Interfaces.Producto;
+using Domain.Enums.Producto;
+
+namespace AppCore.Factories
+{
+    public class ValoracionInventarioFactory
+    {
+        public IValoracionInventario CrearInstancia(TipoValorInventario tipo)
+        {
+            switch (tipo)
+            {
+                case TipoValorInventario.Peps:
+                    break;
+                case TipoValorInventario.Ueps:
+                    break;
+                case TipoValorInventario.PromedioSimple:
+                    break;
+                case TipoValorInventario.PromedioPonderado:
+                    break;
+            }
+            return null;
+        }
+    }
+}
